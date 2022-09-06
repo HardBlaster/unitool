417103d1ba075e24f4cd133c9233ca6da9bc1ec8
hidieric <hidieric@protonmail.com>
2022-09-06 09:30:18 +0200

    less specific annotation processing, cvat reader methods return dicts instead of dataframes.

--- tests/data/test_cvat.py
+++ tests/data/test_cvat.py
@@ -12,13 +12,13 @@ BOXES = DATA_BASE_PATH + 'boxes.pkl'
 class TestCvat(unittest.TestCase):
 
     def test_load_polylines(self):
-        with open(POLYLINES, 'rb') as df_file:
-            parsed_df = pkl.load(df_file)
+        with open(POLYLINES, 'rb') as pl_file:
+            polylines = pkl.load(pl_file)
 
-        self.assertTrue(cvat.load_polylines(ANNOTATIONS).equals(parsed_df))
+        self.assertTrue(cvat.load_polylines(ANNOTATIONS) == polylines)
 
     def test_load_boxes(self):
-        with open(BOXES, 'rb') as df_file:
-            parsed_df = pkl.load(df_file)
+        with open(BOXES, 'rb') as box_file:
+            boxes = pkl.load(box_file)
 
-        self.assertTrue(cvat.load_boxes(ANNOTATIONS).equals(parsed_df))
+        self.assertTrue(cvat.load_boxes(ANNOTATIONS) == boxes)
--- universe/data/cvat.py
+++ universe/data/cvat.py
@@ -1,81 +1,74 @@
+from typing import Dict, List, Tuple
 from xml.etree import ElementTree as et
 
-import pandas as pd
+from universe.iteration import stacked
+from universe.math.geometry import Point2d
 
 
-def load_polylines(path: str) -> pd.DataFrame:
+def load_polylines(path: str, label: str = None) -> Dict[str, List[List[Point2d]]]:
     """
-    CVAT1.1 polyline annotation extractor. Returns a dataframe containing the following columns:
-    - point_count: number of point on the given polyline,
-    - coordinates: list of coordinates (x1,y1,x2,y2,...),
-    - image: image name.
+    CVAT1.1 polyline annotation extractor. Returns a dictionary with image names as keys and a list of polylines as
+    values.
 
+    :param label: load only this label.
     :param path: path to the annotation file.
-    :return: pandas dataframe.
+    :return: dictionary of polylines. Key: image name, value: list of polylines. Polylines are represented as a list of
+    points.
     """
-    polylines = pd.DataFrame(columns=[
-        'point_count',
-        'coordinates',
-        'image'
-    ])
+    polylines = {}
 
     tree = et.parse(path)
     root = tree.getroot()
     for image in root.findall('image'):
         img_name = image.get('name')
+        polylines.update({img_name: []})
 
         for pl in image.findall('polyline'):
+            if label and pl.get('label') != label:
+                continue
 
             # coordinates are stored in textual format (x0,y0;x1,y1;...)
             # for some reason cvat stores coord values in subpixel accuracy -> cast the string to float then to int
             coords = [int(float(coord)) for coord in pl.get('points').replace(';', ',').split(',')]
-            polylines.loc[len(polylines.index)] = [
-                len(coords)//2,
-                coords,
-                img_name
-            ]
+            points = [Point2d(x, y) for x, y in stacked(coords, 2, 2)]
+
+            polylines[img_name].append(points)
 
     return polylines
 
 
-def load_boxes(path: str) -> pd.DataFrame:
+def load_boxes(path: str, label: str = None) -> Dict[str, List[Tuple[Point2d, Point2d]]]:
     """
-    CVAT1.1 box annotation extractor. Returns a dataframe containing the following columns:
-    - x1: top-left corner's X coordinate,
-    - y1: top-left corner's Y coordinate,
-    - x2: bottom-right corner's X coordinate,
-    - y2: bottom-right corner's Y coordinate,
-    - label: annotation label,
-    - image: image name.
+    CVAT1.1 box annotation extractor. Returns a dictionary with image names as keys and a list of tuples containing the
+    top-left and bottom-right points of the boxes as values.
 
+    :param label: load only this label.
     :param path: path to the annotation file.
-    :return: pandas dataframe
+    :return: dictionary of boxes. Key: image name, value: list of tuples.
     """
-    boxes = pd.DataFrame(columns=[
-        'x1',
-        'y1',
-        'x2',
-        'y2',
-        'label',
-        'image'
-    ])
+    boxes = {}
 
     tree = et.parse(path)
     root = tree.getroot()
     for image in root.findall('image'):
         img_name = image.get('name')
+        boxes.update({img_name: []})
 
         for box in image.findall('box'):
+            if label and box.get('label') != label:
+                continue
 
             # coordinates are stored in textual format (x0,y0;x1,y1;...)
             # for some reason cvat stores coord values in subpixel accuracy -> cast the string to float then to int
-            boxes.loc[len(boxes.index)] = [
-                int(float(box.get('xtl'))),
-                int(float(box.get('ytl'))),
-                int(float(box.get('xbr'))),
-                int(float(box.get('ybr'))),
-                box.get('label'),
-                img_name
-            ]
+            boxes[img_name].append((
+                Point2d(
+                    int(float(box.get('xtl'))),
+                    int(float(box.get('ytl'))),
+                ),
+                Point2d(
+                    int(float(box.get('xbr'))),
+                    int(float(box.get('ybr'))),
+                ),
+            ))
 
     return boxes
